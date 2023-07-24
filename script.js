e263e9a6ce96d05f3b02825698afe976cb3720ea
cting0304 <christian.ting0304@gmail.com>
2023-07-24 23:57:40 +0800

    Initial Commit

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ const header = document.querySelector("header");
 window.addEventListener ("scroll", function() {
 	header.classList.toggle ("sticky", window.scrollY > 100);
 });
+
+// Function to toggle the menu
+function toggleMenu() {
+	const navLinks = document.querySelector('.navlist');
+	navLinks.classList.toggle('show');
+  }
+  
+  // Add event listener to the menu icon
+  const menuIcon = document.getElementById('menu-icon');
+  menuIcon.addEventListener('click', toggleMenu);
+  
