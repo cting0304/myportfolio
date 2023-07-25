328bda98b8393fb88f944082cbecd0afb5bff27c
cting0304 <christian.ting0304@gmail.com>
2023-07-25 18:54:39 +0800

    added code

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const header = document.querySelector("header");
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY > 100);
+window.addEventListener("scroll", function () {
+  header.classList.toggle("sticky", window.scrollY > 100);
 });
 
 // Function to toggle the menu
 function toggleMenu() {
-	const navLinks = document.querySelector('.navlist');
-	navLinks.classList.toggle('show');
-  }
-  
-  // Add event listener to the menu icon
-  const menuIcon = document.getElementById('menu-icon');
+  const navLinks = document.querySelector('.navlist');
+  navLinks.classList.toggle('show');
+}
+
+// Add event listener to the menu icon
+const menuIcon = document.getElementById('menu-icon');
+if (menuIcon) {
   menuIcon.addEventListener('click', toggleMenu);
-  
+}
